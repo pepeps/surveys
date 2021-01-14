06666c22090b74ec99e313e1034d018c91d1242c
pepeps <gpe9503@hotmail.com>
2021-01-14 14:01:32 -0600

    install axios, and post data to fake endpoint

--- src/components/surveyDisplay/SurveyTwo.js
+++ src/components/surveyDisplay/SurveyTwo.js
@@ -1,11 +1,20 @@
 import React, { useCallback, useState } from 'react';
 import MySurvey from '../surveyTypes/SurveyTypeTwo';
+import axios from 'axios';
 
 const SurveyTwo = () => {
   const [showPage, setShowPage] = useState(true);
   const onCompletePage = useCallback(
     (data) => {
       console.log(data);
+      axios
+        .post('https://jsonplaceholder.typicode.com/posts', data)
+        .then((res) => {
+          console.log(Response);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
       setShowPage(!showPage);
     },
     [showPage]
